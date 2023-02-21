4f2c38132ab37607e6b50178fbcabc093d27700f
mirsella <mirsella@protonmail.com>
2023-02-21 19:44:51 +0100

    first item of list is empty ""

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lgillard <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 13:53:10 by lgillard          #+#    #+#             */
-/*   Updated: 2023/02/21 19:43:37 by mirsella         ###   ########.fr       */
+/*   Updated: 2023/02/21 19:44:43 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ unsigned char	g_exit_code;
 int	init_shell(t_list **env, char **envp)
 {
 	char	**env_tab;
+	char	*tmp;
 	t_list	*lst;
 
 	g_exit_code = 0;
@@ -34,7 +35,10 @@ int	init_shell(t_list **env, char **envp)
 		if (!(*env))
 			return (perror("malloc"), -1);
 	}
-	lst = ft_lstnew(ft_strdup(""));
+	tmp = ft_strdup("");
+	if (!tmp)
+		return (perror("malloc"), -1);
+	lst = ft_lstnew(tmp);
 	if (!lst)
 		return (perror("malloc"), -1);
 	ft_lstadd_front(env, lst);
