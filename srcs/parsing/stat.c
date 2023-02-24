6e94c57aa47b411cb5140516e94f251562e02179
mirsella <mirsella@protonmail.com>
2023-02-24 15:18:47 +0100

    update redirections rules

--- srcs/parsing/stat.c
+++ srcs/parsing/stat.c
@@ -6,7 +6,7 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 14:16:51 by mirsella          #+#    #+#             */
-/*   Updated: 2023/02/17 16:01:32 by mirsella         ###   ########.fr       */
+/*   Updated: 2023/02/24 15:15:40 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int	is_file_executable(char *path)
 	if (stat(path, &st) == -1)
 		return (0);
 	if (!S_ISREG(st.st_mode))
-		return (print_error(path, "not a regular file"), 0);
+		return (print_error(path, "Not a regular file"), 0);
 	if (!(st.st_mode & S_IXUSR))
-		return (print_error(path, "not executable"), 0);
+		return (print_error(path, "Not executable"), 0);
 	return (1);
 }
 
@@ -33,13 +33,12 @@ int	is_file_readable(char *path)
 
 	if (stat(path, &st) == -1)
 		return (print_error(path, strerror(errno)), 0);
-	if (!S_ISREG(st.st_mode))
-		return (print_error(path, "not a regular file"), 0);
 	if (!(st.st_mode & S_IRUSR))
-		return (print_error(path, "read permission denied"), 0);
+		return (print_error(path, "Read permission denied"), 0);
 	return (1);
 }
 
+// shouldn't be a directory
 int	is_file_writable(char *path)
 {
 	struct stat	st;
@@ -51,9 +50,9 @@ int	is_file_writable(char *path)
 		else
 			return (print_error(path, strerror(errno)), 0);
 	}
-	if (!S_ISREG(st.st_mode))
-		return (print_error(path, "not a regular file"), 0);
+	if (S_ISDIR(st.st_mode))
+		return (print_error(path, "Is a directory"), 0);
 	if (!(st.st_mode & S_IWUSR))
-		return (print_error(path, "write permission denied"), 0);
+		return (print_error(path, "Write permission denied"), 0);
 	return (1);
 }
