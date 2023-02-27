460d195211d1e4473744f86f6c75a38307580cb0
mirsella <mirsella@protonmail.com>
2023-02-27 11:19:50 +0100

    comment debugging printf's

--- srcs/execution/processus.c
+++ srcs/execution/processus.c
@@ -6,7 +6,7 @@
 /*   By: dly <dly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 19:17:06 by dly               #+#    #+#             */
-/*   Updated: 2023/02/27 11:04:47 by mirsella         ###   ########.fr       */
+/*   Updated: 2023/02/27 11:18:31 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static void	child(t_proc *tmp, t_proc *proc, t_list *env)
 	}
 	// printf("path : %s  \n pipe[0]: %d && pipe[1]: %d \n fd_in: %d  && fd_out: %d\n\n", proc->path, proc->pipes[0]
 	// 		,proc->pipes[1], proc->fd_in, proc->fd_out);
-	printf("forking line %s\n", proc->line);
+	// printf("forking line %s\n", proc->line);
 	proc->pid = fork();
 	if (!proc->pid)
 	{
@@ -81,16 +81,16 @@ int	process(t_proc *proc, t_list *env)
 	t_proc	*tmp;
 
 	tmp = proc;
-	if (proc)
-		printf("in process type %d line %s\n", proc->type, proc->line);
-	else
-		printf("proc is NULL\n");
+	// if (proc)
+	// 	printf("in process type %d line %s\n", proc->type, proc->line);
+	// else
+	// 	printf("proc is NULL\n");
 	while (proc)
 	{
 		parse_line_to_proc(proc->line, proc, env);
-		printf("parselinetoproc filled proc type %d line %s and path %s\n", proc->type, proc->line, proc->path);
-		if (proc->type == SUBSHELL)
-			printf("  procs: %p\n", proc->procs);
+		// printf("parselinetoproc filled proc type %d line %s and path %s\n", proc->type, proc->line, proc->path);
+		// if (proc->type == SUBSHELL)
+		// 	printf("  procs: %p\n", proc->procs);
 		// printf("path : %s  line: %s \n pipe[0]: %d && pipe[1]: %d \n fd_in: %d  && fd_out: %d\n\n"
 		// 	,proc->path, proc->line, proc->pipes[0] ,proc->pipes[1], proc->fd_in, proc->fd_out);
 		if (!cmd_not_found(proc) && (proc->path || proc->type == SUBSHELL))
@@ -98,17 +98,14 @@ int	process(t_proc *proc, t_list *env)
 			assign_pipe(proc);
 			if (proc->type == COMMAND && proc->fd_in != -1)
 			{
-				printf("calling child %s\n", proc->line);
+				// printf("calling child %s\n", proc->line);
 				child(tmp, proc, env);
 			}
 			if (proc->type == SUBSHELL)
 			{
 				// assign_pipe_subshell(proc->procs, proc, env);
-				printf("calling process recursively bc of SUBSHELL %s and procs %p\n", proc->line, proc->procs);
+				// printf("calling process recursively bc of SUBSHELL %s and procs %p\n", proc->line, proc->procs);
 				process(proc->procs, env);
-				break ;
-				// if (proc->next_pipeline == AND || proc->next_pipeline == OR)
-				// 	return (recursive_and_or(proc, env, 0), 0);
 			}
 		}
 		if (proc->next_pipeline == AND || proc->next_pipeline == OR)
