3faed0d5fb4f850475a42685ac060be7d6aa016c
Lucas GILLARD <lgillard@e1r10p5.clusters.42paris.fr>
2023-02-28 14:12:36 +0100

    ctrlc during heredoc cancel execution

--- srcs/parsing/heredoc.c
+++ srcs/parsing/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: dly <dly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:51:48 by mirsella          #+#    #+#             */
-/*   Updated: 2023/02/28 14:07:59 by lgillard         ###   ########.fr       */
+/*   Updated: 2023/02/28 14:11:56 by lgillard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ int	set_heredoc_delim(char *line, char **delim, int *expand)
 // 	return (0);
 // }
 
-char	*get_line(char *delim)
+int	get_line(char *delim, char **str)
 {
 	char	*line;
 	char	*tmp;
@@ -91,29 +91,31 @@ char	*get_line(char *delim)
 	if (!line)
 	{
 		if (g_exit_code == 128 + SIGINT)
-			return (NULL);
+			return (1);
 		printf("minishell: warning: %s wanted `%s'\n",
 			"here-document delimited by end-of-file", delim);
-		return (NULL);
+		return (0);
 	}
 	tmp = ft_substr(line, 0, ft_strlen(line));
 	free(line);
 	if (!tmp)
-		return (NULL);
-	return (tmp);
+		return (-1);
+	*str = tmp;
+	return (0);
 }
 
 int	read_until_delim(char *delim, int expand, int fd, t_list *env)
 {
 	char	*line;
 	char	*tmp;
+	int		ret;
 
 	line = "";
 	while (line)
 	{
-		line = get_line(delim);
-		if (!line)
-			return (-1);
+		ret = get_line(delim, &line);
+		if (ret)
+			return (1);
 		if (ft_strcmp(line, delim) == 0)
 			return (free(line), 0);
 		if (expand)
@@ -138,17 +140,16 @@ int	heredoc_redirection(char *line, t_proc *proc, t_list *env)
 	int		ret;
 	int		expand;
 
-	signal(SIGQUIT, SIG_IGN);
-	signal(SIGINT, &sigint_heredoc_handler);
+	ret = 0;
 	ret = set_heredoc_delim(line, &delim, &expand);
 	if (ret)
 		return (ret);
 	if (pipe(pipes) == -1)
 		return (-1);
 	proc->fd_in = pipes[0];
-	read_until_delim(delim, expand, pipes[1], env);
+	ret = read_until_delim(delim, expand, pipes[1], env);
 	free(delim);
 	close(pipes[1]);
 	proc->fd_in = pipes[0];
-	return (0);
+	return (ret);
 }
